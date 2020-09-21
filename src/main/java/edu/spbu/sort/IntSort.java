cd2d0377ebdc5d2f6648dd4f8639f65831b93204
barawecard <barawecard@gmail.com>
2020-09-22 01:09:50 +0300

    quicksort for integers

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,8 +8,37 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
+  static int partition(int arr[], int low, int high)
+  {
+    int pivot = arr[high];
+    int i = (low-1);
+    for (int j=low; j<high; j++)
+    {
+      if (arr[j] < pivot)
+      {
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+    int temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+
+    return i+1;
+  }
+  static void qsort(int arr[], int low, int high)
+  {
+    if (low < high)
+    {
+      int pi = partition(arr, low, high);
+      qsort(arr, low, pi-1);
+      qsort(arr, pi+1, high);
+    }
+  }
   public static void sort (int array[]) {
-    Arrays.sort(array);
+    qsort(array, 0, array.length-1);
   }
 
   public static void sort (List<Integer> list) {
